1a321ecb974167c55f063a80e9e83b05185de600
Caio Martins <caiomart000@gmail.com>
2024-01-04 20:14:08 -0300

    dropdown menu refatorado

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
@@ -12,33 +12,27 @@ import ScrollAnima from './modules/scroll-anima.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
-
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
-
 const tabNav = new TabNav(
   '[data-tab="menu"] li',
   '[data-tab="content"] section',
 );
 tabNav.init();
-
 const modal = new Modal(
   '[data-modal="abrir"]',
   '[data-modal="fechar"]',
   '[data-modal="container"]',
 );
 modal.init();
-
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
-
-initDropdownMenu();
-initMenuMobile();
-initFuncionamento();
-
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
-
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+initMenuMobile();
+initFuncionamento();
